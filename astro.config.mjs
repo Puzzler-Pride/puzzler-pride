d1624d397a12437ffe3eaa82f60bdb5d96dc0ba8
David Millar <dave@davegoesthedistance.com>
2026-05-29 20:55:37 -0500

    Update deps and build cache items.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import { satteri } from '@astrojs/markdown-satteri';
 import tailwindcss from "@tailwindcss/vite";
 import compress from 'astro-compress';
 import icon from "astro-icon";
@@ -10,6 +11,11 @@ export default defineConfig({
   site: 'https://pride.puz.fun',
   compressHTML: true,
   integrations: [mdx(), icon(), compress()],
+    markdown: {
+    processor: satteri({
+      features: { directive: true },
+    }),
+  },
   vite: {
     plugins: [devtoolsJson(), tailwindcss()],
   },
